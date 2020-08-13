390b71cd9cc4141d6d388e33094971f0867924ea
gongdear <gongdear@gmail.com>
2020-08-13 12:41:21 +0800

    init source

---
+++ src/main/java/com/gongdear/geojson/GeoJSON.java
@@ -0,0 +1,41 @@
+package com.gongdear.geojson;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public abstract class GeoJSON {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @JsonProperty("type")
+    private String type;
+
+    @JsonCreator
+    public GeoJSON() {
+        setType(getClass().getSimpleName());
+    }
+
+    public String toString() {
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonGenerationException e) {
+            return "Unhandled exception occured when serializing this instance";
+        } catch (JsonMappingException e) {
+            return "Unhandled exception occured when serializing this instance";
+        } catch (IOException e) {
+            return "Unhandled exception occured when serializing this instance";
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
